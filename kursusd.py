346aad856cc900f5a833cca05a10010e2ab982c6
michelledevina <Michelle.Devina@ti.ukdw.ac.id>
2025-02-08 23:07:13 +0700

    kursusd

---
+++ kursusd.py
@@ -0,0 +1,10 @@
+kursusd = 13950
+
+print('Program konversi US$ ke IDR')
+print('Kurs saat ini 1 US$ = ',kursusd, 'Rupiah')
+
+jumlahusd = float(input('Masukkan jumlah uang yang mau ditukar ke Rupiah: '))
+
+dalamrupiah = jumlahusd * kursusd
+
+print('Hasil konversi = Rp. ', dalamrupiah)
